ac5e8922a9059cddf1ef66e875567ac885e78ef4
Du Gardin Emeric <emeric.du-gardin@epita.fr>
2021-03-28 16:34:55 +0100

    Fix Database Issue

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace dublin_bikes
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Bikes}/{action=Index}/{id?}");
             });
         }
     }
